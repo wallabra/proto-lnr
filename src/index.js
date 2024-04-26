0d40ee1350047027ae4990f6f75d3aa9ed811c2c
Gustavo Rehermann <rehermann6046@gmail.com>
2024-04-26 16:30:54 -0300

    Basic canvas logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 const canvas = document.querySelector("#game-canvas");
+const drawCtx = canvas.getContext('2d');
+
+let fillColor = '#55aaff';
+drawCtx.fillStyle = fillColor;
+
+drawCtx.fillRect(0, 0, canvas.width, canvas.height);
