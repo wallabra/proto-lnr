4246f1c6233bf6bd669e798e164e726825c63e15
Gustavo Rehermann <rehermann6046@gmail.com>
2024-04-29 09:32:50 -0300

    Improve tick loop

--- src/tick.js
+++ src/tick.js
@@ -2,22 +2,29 @@
 const m_render = require('./render.js');
 
 let lastTime = null;
+const frameDuration = 1000 / 60;
 
-export function tick(game: Game, current: Date) {
-  requestAnimationFrame(tick.bind(null, game));
-  
+export function tick(game: Game, current: Date) {  
   if (lastTime == null) {
+    requestAnimationFrame(tick.bind(null, game));
     lastTime = current;
     return;
   }
   
   let deltaTime = (current - lastTime) / 1000;
   lastTime = current;
+  
+  while (deltaTime > frameDuration) {
+    processTick(game, frameDuration);
+    deltaTime -= frameDuration;
+  }
+  
+  requestAnimationFrame(tick.bind(null, game));
 
   processTick(game, deltaTime);
+  m_render.render(game);
 }
 
 function processTick(game: Game, deltaTime: Dates) {
-  m_render.render(game);
   game.tick(deltaTime);
 }
