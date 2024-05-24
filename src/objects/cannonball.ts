c44f238ff6cdaca5ee10879b3b02f217916c3432
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2024-05-24 19:45:28 -0300

    Mildly nerf cannonball damage

--- src/objects/cannonball.ts
+++ src/objects/cannonball.ts
@@ -86,7 +86,7 @@ export class Cannonball {
       this.damageFactor *
         this.phys.kineticEnergyRelativeTo(ship) *
         damageScale *
-        50,
+        30,
     );
     if (ship.dying) {
       this.instigator.killScore++;
