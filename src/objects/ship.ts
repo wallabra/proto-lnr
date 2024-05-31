3461a6a89ea920977239e6e8ff7c59723ef4690c
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2024-05-31 16:24:58 -0300

    Cull rendering ships outside screen

--- src/objects/ship.ts
+++ src/objects/ship.ts
@@ -98,6 +98,197 @@ export function closestCircle(
     );
 }
 
+interface ShipRenderContextArgs {
+  ship: Ship;
+  info: ObjectRenderInfo;
+}
+
+class ShipRenderContext {
+  ship: Ship;
+  ctx: CanvasRenderingContext2D;
+  drawPos: Vec2;
+  info: ObjectRenderInfo;
+  shoffs: number;
+  hoffs: number;
+  scale: number;
+  size: number;
+  isPlayer: boolean;
+  drawScale: number;
+  game: Game;
+
+  constructor(args: ShipRenderContextArgs) {
+    const ship = (this.ship = args.ship);
+    const info = (this.info = args.info);
+    this.ctx = info.ctx;
+    this.drawScale = (this.game = ship.game).drawScale;
+
+    const drawPos = info.base
+      .clone()
+      .add(ship.pos.clone().subtract(info.cam).multiply(info.scaleVec));
+    const camheight = 4;
+    const cdist =
+      (drawPos.clone().subtract(info.base).length() / info.smallEdge) * 0.5;
+    const hdist = camheight - ship.height / 2;
+    const proximityScale = camheight / new Vec2(hdist, cdist).length();
+    const scale = proximityScale * info.scale;
+    const size = ship.size * scale;
+    const isPlayer = ship.isPlayer;
+
+    if (hdist < 0.1) {
+      return;
+    }
+
+    const hoffs = ship.height * 20 + ship.phys.size / 2.5;
+    const shoffs = Math.max(
+      0,
+      hoffs - Math.max(ship.phys.floor, ship.play.waterLevel) * 20,
+    );
+
+    Object.assign(this, { isPlayer, scale, size, drawPos, hoffs, shoffs });
+  }
+
+  drawBody() {
+    const { ctx, drawPos, ship, isPlayer, scale, size, shoffs } = this;
+
+    // Draw shadow
+    ctx.fillStyle = "#0008";
+    ctx.beginPath();
+    ctx.ellipse(
+      drawPos.x,
+      drawPos.y + shoffs,
+      size * ship.lateralCrossSection,
+      size,
+      ship.angle,
+      0,
+      2 * Math.PI,
+    );
+    ctx.fill();
+
+    // Draw body
+    ctx.fillStyle = isPlayer ? "#227766" : "#4a1800";
+    ctx.beginPath();
+    ctx.ellipse(
+      drawPos.x,
+      drawPos.y,
+      size * ship.lateralCrossSection,
+      size,
+      ship.angle,
+      0,
+      2 * Math.PI,
+    );
+    ctx.fill();
+
+    ctx.fillStyle = isPlayer ? "#115533" : "#331100";
+    ctx.beginPath();
+    ctx.ellipse(
+      drawPos.x,
+      drawPos.y,
+      size * ship.lateralCrossSection * 0.8,
+      size * 0.8,
+      ship.angle,
+      0,
+      2 * Math.PI,
+    );
+    ctx.fill();
+
+    // Draw forward direction
+    ctx.strokeStyle = "#08080840";
+    ctx.lineWidth = 1.75;
+    ctx.beginPath();
+    ctx.moveTo(drawPos.x, drawPos.y);
+    const to = new Vec2(ship.size * ship.lateralCrossSection * scale, 0)
+      .rotateBy(ship.angle)
+      .add(drawPos);
+    ctx.lineTo(to.x, to.y);
+    ctx.stroke();
+  }
+
+  drawDamageBar() {
+    const { ctx, drawPos, ship, drawScale } = this;
+
+    const maxDmg = ship.maxDmg;
+    let dmgAlpha = ship.damage / maxDmg;
+
+    if (dmgAlpha > 1) {
+      dmgAlpha = 1;
+    }
+
+    dmgAlpha = 1 - dmgAlpha;
+
+    if (dmgAlpha >= 1) return;
+
+    ctx.fillStyle = "#33AA0088";
+    ctx.fillRect(
+      drawPos.x - 50 * drawScale,
+      drawPos.y - this.size * drawScale - 30 * drawScale,
+      100 * dmgAlpha * drawScale,
+      3,
+    );
+    ctx.fillStyle = "#00000088";
+    ctx.fillRect(
+      drawPos.x - 50 * drawScale + 100 * dmgAlpha * drawScale,
+      drawPos.y - this.size * drawScale - 30 * drawScale,
+      100 * drawScale * (1 - dmgAlpha),
+      3,
+    );
+  }
+
+  drawDebug() {
+    const { ctx, info, ship, scale } = this;
+
+    const from = ship.pos
+      .clone()
+      .add(
+        new Vec2(ship.size * ship.lateralCrossSection, 0).rotateBy(ship.angle),
+      );
+    const fromDraw = info.base
+      .clone()
+      .add(from.clone().subtract(info.cam).multiplyScalar(scale));
+    const angmom = ship.phys.orbitalVelocityAt(from).multiplyScalar(scale);
+
+    // Draw angular velocity
+    ctx.strokeStyle = "#fa09";
+    ctx.beginPath();
+    ctx.moveTo(fromDraw.x, fromDraw.y);
+    ctx.lineTo(fromDraw.x + angmom.x, fromDraw.y + angmom.y);
+    ctx.stroke();
+
+    // Draw velocity
+    ctx.strokeStyle = "#06fc";
+    ctx.beginPath();
+    ctx.moveTo(fromDraw.x, fromDraw.y);
+    const vto = ship.vel.multiplyScalar(scale * 10).add(fromDraw);
+    ctx.lineTo(vto.x, vto.y);
+    ctx.stroke();
+
+    // Draw collision circles
+    const circles = ship.collisionCircles();
+
+    ctx.strokeStyle = "#0f0c";
+    ctx.lineWidth = 1.5;
+    for (const circle of circles) {
+      const center = info.base
+        .clone()
+        .add(circle.center.clone().subtract(info.cam).multiplyScalar(scale));
+      const size = circle.radius * scale;
+      ctx.beginPath();
+      ctx.ellipse(center.x, center.y, size, size, 0, 0, Math.PI * 2);
+      ctx.stroke();
+    }
+  }
+
+  draw() {
+    if (!this.ship.isVisible(this.info)) return;
+
+    this.drawBody();
+    this.drawDamageBar();
+
+    if (DEBUG_DRAW) {
+      this.drawDebug();
+    }
+  }
+}
+
 export class Ship {
   game: Game;
   phys: PhysicsObject;
@@ -161,7 +352,7 @@ export class Ship {
   scoreKill() {
     this.killScore++;
 
-    if ((this.game.player != null) & (this.game.player.possessed === this)) {
+    if (this.game.player != null && this.game.player.possessed === this) {
       this.game.player.kills++;
     }
   }
@@ -302,7 +493,9 @@ export class Ship {
   }
 
   isVisible(info: ObjectRenderInfo) {
-    const pos = this.pos.clone().subtract(info.cam).multiply(info.scaleVec);
+    const pos = info.base
+      .clone()
+      .add(this.pos.clone().subtract(info.cam).multiplyScalar(info.scale));
     const edge = this.size * this.lateralCrossSection;
 
     return (
@@ -314,163 +507,22 @@ export class Ship {
   }
 
   render(info: ObjectRenderInfo) {
-    const ctx = info.ctx;
-    const drawScale = this.game.drawScale;
+    const drawer = new ShipRenderContext({
+      ship: this,
+      info: info,
+    });
 
-    const drawPos = info.base
-      .clone()
-      .add(this.pos.clone().subtract(info.cam).multiply(info.scaleVec));
-    const camheight = 4;
-    const cdist =
-      (drawPos.clone().subtract(info.base).length() / info.smallEdge) * 0.5;
-    const hdist = camheight - this.height / 2;
-    const proximityScale = camheight / new Vec2(hdist, cdist).length();
-    const scale = proximityScale * info.scale;
-    const size = this.size * scale;
-    const isPlayer = this.isPlayer;
-
-    if (hdist < 0.1) {
-      return;
-    }
-
-    const hoffs = this.height * 20 + this.phys.size / 2.5;
-    const shoffs = Math.max(
-      0,
-      hoffs - Math.max(this.phys.floor, this.play.waterLevel) * 20,
-    );
-
-    // Draw shadow
-    ctx.fillStyle = "#0008";
-    ctx.beginPath();
-    ctx.ellipse(
-      drawPos.x,
-      drawPos.y + shoffs,
-      size * this.lateralCrossSection,
-      size,
-      this.angle,
-      0,
-      2 * Math.PI,
-    );
-    ctx.fill();
-
-    // Draw body
-    ctx.fillStyle = isPlayer ? "#227766" : "#4a1800";
-    ctx.beginPath();
-    ctx.ellipse(
-      drawPos.x,
-      drawPos.y,
-      size * this.lateralCrossSection,
-      size,
-      this.angle,
-      0,
-      2 * Math.PI,
-    );
-    ctx.fill();
-
-    ctx.fillStyle = isPlayer ? "#115533" : "#331100";
-    ctx.beginPath();
-    ctx.ellipse(
-      drawPos.x,
-      drawPos.y,
-      size * this.lateralCrossSection * 0.8,
-      size * 0.8,
-      this.angle,
-      0,
-      2 * Math.PI,
-    );
-    ctx.fill();
-
-    // Draw forward direction
-    ctx.strokeStyle = "#08080840";
-    ctx.lineWidth = 1.75;
-    ctx.beginPath();
-    ctx.moveTo(drawPos.x, drawPos.y);
-    const to = new Vec2(this.size * this.lateralCrossSection * scale, 0)
-      .rotateBy(this.angle)
-      .add(drawPos);
-    ctx.lineTo(to.x, to.y);
-    ctx.stroke();
-
-    if (DEBUG_DRAW) {
-      const from = this.pos
-        .clone()
-        .add(
-          new Vec2(this.size * this.lateralCrossSection, 0).rotateBy(
-            this.angle,
-          ),
-        );
-      const fromDraw = info.base
-        .clone()
-        .add(from.clone().subtract(info.cam).multiplyScalar(scale));
-      const angmom = this.phys.orbitalVelocityAt(from).multiplyScalar(scale);
-
-      // Draw angular velocity
-      ctx.strokeStyle = "#fa09";
-      ctx.beginPath();
-      ctx.moveTo(fromDraw.x, fromDraw.y);
-      ctx.lineTo(fromDraw.x + angmom.x, fromDraw.y + angmom.y);
-      ctx.stroke();
-
-      // Draw velocity
-      ctx.strokeStyle = "#06fc";
-      ctx.beginPath();
-      ctx.moveTo(fromDraw.x, fromDraw.y);
-      const vto = this.vel.multiplyScalar(scale * 10).add(fromDraw);
-      ctx.lineTo(vto.x, vto.y);
-      ctx.stroke();
-
-      // Draw collision circles
-      const circles = this.collisionCircles();
-
-      ctx.strokeStyle = "#0f0c";
-      ctx.lineWidth = 1.5;
-      for (const circle of circles) {
-        const center = info.base
-          .clone()
-          .add(circle.center.clone().subtract(info.cam).multiplyScalar(scale));
-        const size = circle.radius * scale;
-        ctx.beginPath();
-        ctx.ellipse(center.x, center.y, size, size, 0, 0, Math.PI * 2);
-        ctx.stroke();
-      }
-    }
-
-    // Draw damage bar
-    const maxDmg = this.maxDmg;
-    let dmgAlpha = this.damage / maxDmg;
-
-    if (dmgAlpha > 1) {
-      dmgAlpha = 1;
-    }
-
-    dmgAlpha = 1 - dmgAlpha;
-
-    if (dmgAlpha < 1) {
-      ctx.fillStyle = "#33AA0088";
-      ctx.fillRect(
-        drawPos.x - 50 * drawScale,
-        drawPos.y - this.size * drawScale - 30 * drawScale,
-        100 * dmgAlpha * drawScale,
-        3,
-      );
-      ctx.fillStyle = "#00000088";
-      ctx.fillRect(
-        drawPos.x - 50 * drawScale + 100 * dmgAlpha * drawScale,
-        drawPos.y - this.size * drawScale - 30 * drawScale,
-        100 * drawScale * (1 - dmgAlpha),
-        3,
-      );
-    }
+    drawer.draw();
   }
 
-  setMoney(money) {
+  setMoney(money: number) {
     this.money = money;
     if (this.game.player != null && this.game.player.possessed === this) {
       this.game.player.money = this.money;
     }
   }
 
-  giveMoney(money) {
+  giveMoney(money: number) {
     this.setMoney(money + this.money);
   }
 
@@ -478,7 +530,7 @@ export class Ship {
     return this.pos
       .clone()
       .add(
-        Vec2(Math.random() * this.size * 0.8, 0).rotateBy(
+        new Vec2(Math.random() * this.size * 0.8, 0).rotateBy(
           Math.random() * Math.PI * 2,
         ),
       );
@@ -487,7 +539,7 @@ export class Ship {
   private pickupParams(): Partial<PhysicsParams> {
     return {
       vel: this.vel.add(
-        Vec2(0.3, 0).rotateBy(random.uniform(0, Math.PI * 2)()),
+        new Vec2(0.3, 0).rotateBy(random.uniform(0, Math.PI * 2)()),
       ),
       vspeed: 0.5,
       height: this.height + 0.1,
@@ -561,10 +613,10 @@ export class Ship {
   }
 
   get steerForce() {
-    return ((0.2 + this.vel.length) * Math.PI) / 6;
+    return ((0.2 + this.vel.length()) * Math.PI) / 6;
   }
 
-  steer(deltaTime, angleTarg) {
+  steer(deltaTime: number, angleTarg: number) {
     let angOffs = angDiff(this.angle, angleTarg);
     const steerForce = this.steerForce;
     const steerCompensate = angDiff(
@@ -580,7 +632,7 @@ export class Ship {
     this.phys.angVel += angOffs * deltaTime;
   }
 
-  steerToward(deltaTime, otherPos) {
+  steerToward(deltaTime: number, otherPos: Vec2) {
     const angleTarg = otherPos.clone().subtract(this.pos).angle();
     this.steer(deltaTime, angleTarg);
   }
@@ -589,7 +641,7 @@ export class Ship {
     return this.makeup.maxEngineThrust(enginesList);
   }
 
-  thrustForward(deltaTime, amount) {
+  thrustForward(deltaTime: number, amount: number) {
     if (amount > 1) {
       amount = 1;
     } else if (amount < -1) {
@@ -612,11 +664,11 @@ export class Ship {
   }
 
   heightGradient() {
-    if (this.play.terrain == null) return Vec2(0, 0);
+    if (this.play.terrain == null) return new Vec2(0, 0);
     return this.play.terrain.gradientAt(this.pos.x, this.pos.y);
   }
 
-  nearShip(ship) {
+  nearShip(ship: Ship) {
     const r1 = this.size * this.lateralCrossSection;
     const r2 = ship.size * ship.lateralCrossSection;
 
@@ -625,7 +677,7 @@ export class Ship {
     return dist <= r1 + r2;
   }
 
-  intermediaryRadius(angle) {
+  intermediaryRadius(angle: number) {
     angle = ((angle - this.angle + Math.PI) % (Math.PI * 2)) - Math.PI;
     return (
       (this.size * this.size * this.lateralCrossSection) /
@@ -637,7 +689,7 @@ export class Ship {
     );
   }
 
-  boundaryPoint(angle) {
+  boundaryPoint(angle: number) {
     return this.pos.add(
       new Vec2(this.intermediaryRadius(angle), 0).rotateBy(angle),
     );
@@ -709,7 +761,7 @@ export class Ship {
       .toArray();
   }
 
-  checkShipCollision(deltaTime, ship) {
+  checkShipCollision(deltaTime: number, ship: Ship) {
     const collision = this.collision(ship);
     if (collision == null) {
       return;
@@ -775,7 +827,7 @@ export class Ship {
     );
   }
 
-  checkShipCollisions(deltaTime) {
+  checkShipCollisions(deltaTime: number) {
     for (const ship of this.play.tickables) {
       if (!(ship instanceof Ship)) {
         continue;
@@ -789,7 +841,7 @@ export class Ship {
     }
   }
 
-  checkTerrainDamage(deltaTime) {
+  checkTerrainDamage(deltaTime: number) {
     if (this.phys.floor > this.play.waterLevel) {
       this.damageShip(1000 * deltaTime);
     }
@@ -806,13 +858,13 @@ export class Ship {
     return this.pos
       .clone()
       .add(
-        Vec2(this.size * this.lateralCrossSection * 0.4, 0).rotateBy(
+        new Vec2(this.size * this.lateralCrossSection * 0.4, 0).rotateBy(
           this.angle,
         ),
       );
   }
 
-  shotAirtime(deltaTime, dist) {
+  shotAirtime(deltaTime: number, dist: number) {
     const cannon = this.makeup.nextReadyCannon;
     if (cannon == null) return null;
     return cannon.airtime(deltaTime, this, dist);
@@ -826,7 +878,7 @@ export class Ship {
     this.phys.weight = this.computeWeight();
   }
 
-  tick(deltaTime) {
+  tick(deltaTime: number) {
     this.updateWeight();
     this.processTickActions(deltaTime);
     this.makeup.tick(deltaTime);
