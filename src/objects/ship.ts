e5866d08c4aacb5a238e39d34b9a3ba557a6f382
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2024-10-18 22:41:41 -0300

    ship: Skip takeDamage calls on zero damage

--- src/objects/ship.ts
+++ src/objects/ship.ts
@@ -852,6 +852,8 @@ export class Ship implements Tickable, Renderable, Damageable {
 
   public takeDamage(damage: number) {
     damage = Math.max(0, damage);
+    if (damage === 0) return;
+    
     const die = this.makeup.damageShip(damage);
 
     if (this.game.player != null && this.game.player.possessed === this) {
