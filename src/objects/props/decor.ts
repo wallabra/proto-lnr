e30ce1448c9f169875484322fd1e90c036f93263
Gustavo Ramos Rehermann <rehermann6046@gmail.com>
2024-10-19 16:51:19 -0300

    props: Support randomized scale

--- src/objects/props/decor.ts
+++ src/objects/props/decor.ts
@@ -1,7 +1,8 @@
 import type { ObjectRenderInfo, Renderable } from "../../render";
 import type { PlayState, Tickable } from "../../superstates/play";
 import type { PhysicsObject, PhysicsParams } from "../physics";
-import { rwc } from "../../util";
+import type { RandomRange } from "../../util";
+import { maybeRange, rwc } from "../../util";
 import type Victor from "victor";
 
 export interface DecorArgs extends Partial<PhysicsParams> {
@@ -15,11 +16,29 @@ import SPRITE_FLAG from "data-url:../../sprites/flag.png";
 
 const SPRITES: Record<
   string,
-  { src: string; angleRandom: boolean; offset?: [number, number] }
+  {
+    src: string;
+    angleRandom: boolean | RandomRange;
+    offset?: [number, number];
+    drawScale?: number | RandomRange;
+  }
 > = {
-  "grass.png": { src: SPRITE_GRASS, angleRandom: true },
-  "rock.png": { src: SPRITE_ROCK, angleRandom: true },
-  "flag.png": { src: SPRITE_FLAG, angleRandom: false, offset: [0, -0.5] },
+  "grass.png": {
+    src: SPRITE_GRASS,
+    angleRandom: true,
+    drawScale: { min: 0.3, max: 0.9 },
+  },
+  "rock.png": {
+    src: SPRITE_ROCK,
+    angleRandom: true,
+    drawScale: { min: 0.4, max: 1.7 },
+  },
+  "flag.png": {
+    src: SPRITE_FLAG,
+    angleRandom: false,
+    offset: [0, -0.5],
+    drawScale: { min: 0.3, max: 0.5 },
+  },
 };
 
 const SPRITE_CACHE = new Map<string, HTMLImageElement>();
@@ -33,22 +52,28 @@ function randomDecor(): string {
 }
 
 export class Decor implements Renderable, Tickable {
-  phys: PhysicsObject;
-  play: PlayState;
-  dying: boolean;
-  drawScale: number;
-  spritePath: string;
-  sprite: HTMLImageElement;
-  args: DecorArgs | undefined;
+  public phys: PhysicsObject;
+  protected play: PlayState;
+  public dying: boolean;
+  protected drawScale: number;
+  protected spritePath: string;
+  protected sprite: HTMLImageElement;
+  protected args: DecorArgs | undefined;
   type = "decor";
+  private setSize: boolean;
 
   constructor(play: PlayState, pos: Victor, args: DecorArgs) {
     this.play = play;
-    this.drawScale = args.drawScale ??= 0.5;
     this.spritePath = args.sprite ??= randomDecor();
+    const def = SPRITES[this.spritePath];
+
+    this.drawScale = args.drawScale ??= maybeRange(def.drawScale ?? 0.5);
     this.args = args;
-    if (args.angle == null && SPRITES[this.spritePath].angleRandom)
-      args.angle = Math.random() * Math.PI * 2;
+    if (args.angle == null && def.angleRandom)
+      args.angle =
+        typeof def.angleRandom !== "boolean"
+          ? maybeRange(def.angleRandom)
+          : Math.random() * Math.PI * 2;
     this.preloadSprite();
     delete this.args;
     this.phys = play.makePhysObj(pos, args);
@@ -78,23 +103,7 @@ export class Decor implements Renderable, Tickable {
 
   render(info: ObjectRenderInfo): void {
     if (!this.sprite.complete) return;
-
-    // use Pythagoras to get minimum radius to encircle the rectangular sprite
     const def = SPRITES[this.spritePath];
-    this.phys.size = Math.sqrt(
-      Math.pow(
-        this.sprite.width *
-          (0.5 + Math.abs(def.offset != null ? def.offset[0] : 0)) *
-          this.drawScale,
-        2,
-      ) +
-        Math.pow(
-          this.sprite.height *
-            (0.5 + Math.abs(def.offset != null ? def.offset[1] : 0)) *
-            this.drawScale,
-          2,
-        ),
-    );
 
     if (!this.phys.isVisible(info)) return;
 
@@ -141,5 +150,25 @@ export class Decor implements Renderable, Tickable {
     // ctx.stroke();
   }
 
-  tick(_deltaTime: number): void {}
+  tick(_deltaTime: number): void {
+    // use Pythagoras to get minimum radius to encircle the rectangular sprite
+    if (this.sprite.complete && !this.setSize) {
+      const def = SPRITES[this.spritePath];
+      this.phys.size = Math.sqrt(
+        Math.pow(
+          this.sprite.width *
+            (0.5 + Math.abs(def.offset != null ? def.offset[0] : 0)) *
+            this.drawScale,
+          2,
+        ) +
+          Math.pow(
+            this.sprite.height *
+              (0.5 + Math.abs(def.offset != null ? def.offset[1] : 0)) *
+              this.drawScale,
+            2,
+          ),
+      );
+      this.setSize = true;
+    }
+  }
 }
